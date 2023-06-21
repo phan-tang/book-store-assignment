cc32bcf5f16167741978ea9b14b97822c5cd8b64
Tang Hien Phan <phan05042000@gmail.com>
2023-06-21 14:51:22 +0700

    Add app's constants

---
+++ book-store-ui/src/app/shared/constants/app.constants.ts
@@ -0,0 +1,5 @@
+export const unit: string = '$';
+export const appName: string = 'Book Store';
+export const appIconName: string = 'menu_book';
+
+export const apiUrl: string = 'http://127.0.0.1:8000/api/';
