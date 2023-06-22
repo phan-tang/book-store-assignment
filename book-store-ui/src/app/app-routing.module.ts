b3f5eeb56e2881fea43316407e0beb1547733cf7
Tang Hien Phan <phan05042000@gmail.com>
2023-06-22 22:58:57 +0700

    Setup app module; use environment variable; add main layout

--- book-store-ui/src/app/app-routing.module.ts
+++ book-store-ui/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/layout/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/app/books',
+    pathMatch: 'full'
+  },
+  {
+    path: 'app',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'books',
+        loadChildren: () => import('./books/books.module').then(m => m.BooksModule)
+      },
+      {
+        path: 'cart',
+        loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
+      },
+    ]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- book-store-ui/src/app/app.module.ts
+++ book-store-ui/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    SharedModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
