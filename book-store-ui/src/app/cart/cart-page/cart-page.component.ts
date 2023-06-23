6dc202c58fc7b033e6d2d87e5981811da59d880f
Tang Hien Phan <phan05042000@gmail.com>
2023-06-23 18:24:59 +0700

    Add code to get cart total

--- book-store-ui/src/app/cart/cart-page/cart-page.component.ts
+++ book-store-ui/src/app/cart/cart-page/cart-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 import { BookListData } from 'src/app/books/shared/book';
-import { FormControl, FormGroup } from '@angular/forms';
+
+import { unit } from 'src/app/shared/constants/app.constants';
+import { totalmem } from 'os';
 
 @Component({
   selector: 'app-cart-page',
@@ -11,16 +13,27 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class CartPageComponent implements OnInit {
   displayedColumns: string[] = ['name', 'final_price', 'quantity'];
   cartItems: BookListData | null = null;
+  cartTotal: string = '0';
   discountCode: string = '';
+  unit: string = unit;
   constructor(private service: CartService) { }
 
   ngOnInit(): void {
     this.service.getBooksInCart().subscribe((data: BookListData) => {
       this.cartItems = data;
+      this.cartTotal = this.getCartTotal(data);
     });
   }
 
   applyDiscountCode() {
     console.log(this.discountCode);
+  }
+
+  getCartTotal(data: BookListData) {
+    let total = 0;
+    data.data.forEach(item => {
+      total += item.quantity * parseFloat(item.final_price);
+    })
+    return total.toFixed(2);
   }
 }
