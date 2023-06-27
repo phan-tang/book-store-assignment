96dd1420ec4838e9dd71a8ffda483f361859606d
Tang Hien Phan <phan05042000@gmail.com>
2023-06-27 15:52:26 +0700

    Add file to connect to database

---
+++ book-store-api/config/database.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+export default class Database {
+    constructor() { }
+
+    async connectToDatabase(): Promise<void> {
+        try {
+            await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+            console.info('Connected to the database');
+        } catch (error) {
+            console.error('Error connecting to the database:', error);
+        }
+    }
+
+    async disconnectFromDatabase(): Promise<void> {
+        try {
+            await mongoose.disconnect();
+            console.log('Disconnected from the database');
+        } catch (error) {
+            console.error('Error disconnecting from the database:', error);
+        }
+    }
+}
+
+export { mongoose };
