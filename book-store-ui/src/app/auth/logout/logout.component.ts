d8dafd89abb66086b60114fd7330654e84d407e9
Tang Hien Phan <phan05042000@gmail.com>
2023-07-10 18:11:51 +0700

    Add logout component

---
+++ book-store-ui/src/app/auth/logout/logout.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss']
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(private service: AuthService, private toastrService: ToastrService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.service.logout().subscribe(({ message }) => {
+      localStorage.clear();
+      this.toastrService.success(message);
+      this.router.navigate(['login'], { relativeTo: this.route.parent });
+    });
+  }
+}
